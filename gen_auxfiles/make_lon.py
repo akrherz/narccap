b0ff516d774d718f709c20cab85f1a30a8fc1b6b
akrherz <akrherz@iastate.edu>
2013-04-25 16:15:49 -0500

    Add scripts and tables necessary to generate table 4 files

---
+++ gen_auxfiles/make_lon.py
@@ -0,0 +1,57 @@
+"""
+Generate the table 4 variable lon
+"""
+import netCDF4
+import datetime
+import numpy
+
+fn = 'lon_MM5I.nc'
+nc = netCDF4.Dataset(fn, 'w', format='NETCDF3_CLASSIC')
+nc.Conventions = 'CF-1.0'
+nc.title = 'ISU MM5 model output prepared for NARCCAP'
+nc.history = 'python netCDF routines, %s' % (datetime.datetime.now().strftime("%Y-%m-%d %H:%M"),)
+nc.contact1 = 'Daryl Herzmann (akrherz@iastate.edu)'
+nc.contact2 = '3015 Agronomy Hall, Iowa State Univ.,Ames, Iowa, USA'
+nc.realization = '1'
+nc.experiment_id = 'all'
+nc.table_id = 'Table 4'
+nc.project_id = 'NARCCAP'
+nc.source = 'MM5(2002): atmosphere: MM5v3.6.3 non-hydrostatic; sst/sea ice: AMIPII; land: Noah;  Convection: Kain-Fritsch 2; Radiation: RRTM; PBL: MRF; Explicit Moisture: Reisner Mixed-Phase; Buffer: 15 point exponential; Horizontal Resolution: 50km; Vertical Levels: 24'
+nc.institution = 'ISU (Iowa State University, Ames, Iowa, USA)'
+
+nc.createDimension('xc', 124)
+nc.createDimension('yc', 99)
+
+xc = nc.createVariable('xc', 'd', ('xc',))
+xc.long_name = 'x-coordinate Cartesian system'
+xc.standard_name = 'projection_x_coordinate'
+xc.axis = 'X'
+xc.units = 'm'
+
+yc = nc.createVariable('yc', 'd', ('yc',))
+yc.long_name = 'y-coordinate Cartesian system'
+yc.standard_name = 'projection_y_coordinate'
+yc.axis = 'Y'
+yc.units = 'm'
+
+lon = nc.createVariable('lon', 'd', ('yc', 'xc'))
+lon.long_name = 'longitude'
+lon.standard_name = 'longitude'
+lon.units = 'degrees_east'
+
+p = nc.createVariable('Lambert_Conformal', 'c', ())
+p.grid_mapping_name = "lambert_conformal_conic"
+p.false_easting = 3825000.
+p.false_northing = 3187500.
+
+nc2 = netCDF4.Dataset('MMOUTP_DOMAIN1_1002.nc')
+
+lon[:] = nc2.variables['longicrs'][15:-15,15:-15] + 360.0
+xc[:] = numpy.arange(15,139) * nc2.variables['grid_ds'][:] * 1000.0
+yc[:] = numpy.arange(15,114) * nc2.variables['grid_ds'][:] * 1000.0
+p.standard_parallel = [nc2.variables['stdlat_2'][:], nc2.variables['stdlat_1'][:]]
+p.longitude_of_central_meridian = nc2.variables['coarse_cenlon'][:]
+p.latitude_of_projection_origin = nc2.variables['coarse_cenlat'][:]
+
+nc2.close()
+nc.close()
---
+++ gen_auxfiles/make_rootd.py
@@ -0,0 +1,92 @@
+"""
+Generate the table 4 variable rootd
+"""
+import netCDF4
+import datetime
+import numpy
+import mm5_class
+
+fn = 'rootd_MM5I.nc'
+nc = netCDF4.Dataset(fn, 'w', format='NETCDF3_CLASSIC')
+nc.Conventions = 'CF-1.0'
+nc.title = 'ISU MM5 model output prepared for NARCCAP'
+nc.history = 'python netCDF routines, %s' % (datetime.datetime.now().strftime("%Y-%m-%d %H:%M"),)
+nc.contact1 = 'Daryl Herzmann (akrherz@iastate.edu)'
+nc.contact2 = '3015 Agronomy Hall, Iowa State Univ.,Ames, Iowa, USA'
+nc.realization = '1'
+nc.experiment_id = 'all'
+nc.table_id = 'Table 4'
+nc.project_id = 'NARCCAP'
+nc.source = 'MM5(2002): atmosphere: MM5v3.6.3 non-hydrostatic; sst/sea ice: AMIPII; land: Noah;  Convection: Kain-Fritsch 2; Radiation: RRTM; PBL: MRF; Explicit Moisture: Reisner Mixed-Phase; Buffer: 15 point exponential; Horizontal Resolution: 50km; Vertical Levels: 24'
+nc.institution = 'ISU (Iowa State University, Ames, Iowa, USA)'
+
+nc.createDimension('xc', 124)
+nc.createDimension('yc', 99)
+
+xc = nc.createVariable('xc', 'd', ('xc',))
+xc.long_name = 'x-coordinate Cartesian system'
+xc.standard_name = 'projection_x_coordinate'
+xc.axis = 'X'
+xc.units = 'm'
+
+yc = nc.createVariable('yc', 'd', ('yc',))
+yc.long_name = 'y-coordinate Cartesian system'
+yc.standard_name = 'projection_y_coordinate'
+yc.axis = 'Y'
+yc.units = 'm'
+
+lat = nc.createVariable('lat', 'd', ('yc', 'xc'))
+lat.long_name = 'latitude'
+lat.standard_name = 'latitude'
+lat.units = 'degrees_north'
+
+lon = nc.createVariable('lon', 'd', ('yc', 'xc'))
+lon.long_name = 'longitude'
+lon.standard_name = 'longitude'
+lon.units = 'degrees_east'
+
+p = nc.createVariable('Lambert_Conformal', 'c', ())
+p.grid_mapping_name = "lambert_conformal_conic"
+p.false_easting = 3825000.
+p.false_northing = 3187500.
+
+v = nc.createVariable('rootd', 'f', ('yc','xc'), fill_value=1e20 )
+v.units = 'm' 
+v.standard_name = 'root_depth'
+v.missing_value = numpy.array(1e20, v.dtype)
+v.coordinates = 'lon lat'
+v.grid_mapping = 'Lambert_Conformal'
+v.long_name = 'Root Depth'
+nc2 = netCDF4.Dataset('MMOUTP_DOMAIN1_1002.nc')
+
+lat[:] = nc2.variables['latitcrs'][15:-15,15:-15]
+lon[:] = nc2.variables['longicrs'][15:-15,15:-15] + 360.0
+xc[:] = numpy.arange(15,139) * nc2.variables['grid_ds'][:] * 1000.0
+yc[:] = numpy.arange(15,114) * nc2.variables['grid_ds'][:] * 1000.0
+p.standard_parallel = [nc2.variables['stdlat_2'][:], nc2.variables['stdlat_1'][:]]
+p.longitude_of_central_meridian = nc2.variables['coarse_cenlon'][:]
+p.latitude_of_projection_origin = nc2.variables['coarse_cenlat'][:]
+
+lookup = {}
+table = open('VEGPARM.TBL', 'r').readlines()
+for line in table[3:30]:
+    tokens = line.split(',')
+    lookup[ int(tokens[0])] = float(tokens[4])
+
+mm5 = mm5_class.mm5('MMINPUT_DOMAIN1.196801')
+data = mm5.get_field('land_use', 0)['values']
+data = numpy.array(data[0])
+
+lookup2 = {0: 0, 1: 0.1, 2: 0.4, 3: 1.0, 4: 2.0}
+
+shp = numpy.shape(data)
+sm = numpy.zeros( shp, 'f')
+for i in range(shp[0]):
+    for j in range(shp[1]):
+        # 1.8 meter soil depth x SMC
+        sm[i,j] = lookup2[ lookup[ data[i,j] ] ]
+
+v[:] = sm[16:-15,16:-15]
+
+nc2.close()
+nc.close()
